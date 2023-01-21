00311ad6d21a0ab20e381bbacc4b25614bd9d807
Ravishankar Rajappa <revysh@gmail.com>
2023-01-21 17:37:02 +0530

    Adding the Immediate retry policy using polly

--- PollyTest.WebAPI/Controllers/CatalogController.cs
+++ PollyTest.WebAPI/Controllers/CatalogController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.Text.Json.Serialization;
+using Polly.Retry;
 
 namespace PollyTest.WebAPI.Controllers
 {
@@ -8,15 +8,29 @@ namespace PollyTest.WebAPI.Controllers
     [Route("[controller]")]
     public class CatalogController : Controller
     {
+        readonly AsyncRetryPolicy<HttpResponseMessage> _httpRetryPolicy;
+
+        public CatalogController()
+        {
+            int retryCount = 0;
+            //Immediate retry policy
+            //_httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+            //    .RetryAsync(3, onRetry: (response, timeSpan) => {
+            //        Console.WriteLine(retryCount++);
+            //    });
+        }
+
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
             var httpClient = new HttpClient();
             string requestEndpoint = "https://" + Request.Host.Value + $"/inventory/{id}";
 
-            HttpResponseMessage response = await httpClient.GetAsync(requestEndpoint);
+            //HttpResponseMessage response = await httpClient.GetAsync(requestEndpoint);
+            HttpResponseMessage response = await _httpRetryPolicy.ExecuteAsync(() => httpClient.GetAsync(requestEndpoint));
 
-            if(response.IsSuccessStatusCode) {
+            if (response.IsSuccessStatusCode)
+            {
                 int itemsInStock = JsonConvert.DeserializeObject<int>(await response.Content.ReadAsStringAsync());
                 return Ok(itemsInStock);
             }
