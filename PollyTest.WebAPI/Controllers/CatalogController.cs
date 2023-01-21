0df43024c6bc3903be5ec92a906659e7b574179f
Ravishankar Rajappa <revysh@gmail.com>
2023-01-21 13:25:19 +0530

    Initil checkin

---
+++ PollyTest.WebAPI/Controllers/CatalogController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace PollyTest.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : Controller
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var httpClient = new HttpClient();
+            string requestEndpoint = "https://" + Request.Host.Value + $"/inventory/{id}";
+
+            HttpResponseMessage response = await httpClient.GetAsync(requestEndpoint);
+
+            if(response.IsSuccessStatusCode) {
+                int itemsInStock = JsonConvert.DeserializeObject<int>(await response.Content.ReadAsStringAsync());
+                return Ok(itemsInStock);
+            }
+
+            return StatusCode((int)response.StatusCode, response.Content.ReadAsStringAsync());
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PollyTest.WebAPI/Controllers/InventoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace PollyTest.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InventoryController : Controller
+    {
+        static int _requestCount = 0;
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            await Task.Delay(100);
+            _requestCount++;
+            if(_requestCount%4 == 0)
+            {
+                return Ok(15);
+            }
+            return StatusCode((int)HttpStatusCode.InternalServerError, "Something went wrong.");
+        }
+    }
+}
