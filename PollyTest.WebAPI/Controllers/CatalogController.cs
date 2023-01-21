d39ce1a02106d3caae8f2bc0bba126980743a61e
Ravishankar Rajappa <revysh@gmail.com>
2023-01-21 18:28:38 +0530

    Adding Exponential wait and retry policy with a fallback policy

--- PollyTest.WebAPI/Controllers/CatalogController.cs
+++ PollyTest.WebAPI/Controllers/CatalogController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Polly;
+using Polly.Fallback;
 using Polly.Retry;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
 
 namespace PollyTest.WebAPI.Controllers
 {
@@ -10,33 +14,46 @@ namespace PollyTest.WebAPI.Controllers
     public class CatalogController : Controller
     {
         readonly AsyncRetryPolicy<HttpResponseMessage> _httpRetryPolicy;
+        readonly AsyncFallbackPolicy<HttpResponseMessage> _httpFallbackPolicy;
+        private int fallBackNumber = 21;
 
         public CatalogController()
         {
-            //int retryCount = 0;
-            ////Immediate retry policy
+            int maxRetryCount = 3;
+            int retryCount = 0;
+            //Immediate retry policy
+            _httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                .RetryAsync(maxRetryCount, onRetry: (response, timeSpan) =>
+                {
+                    Console.WriteLine(retryCount++);
+                });
+
+            ////Exponential wait and retry policy
             //_httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
-            //    .RetryAsync(3, onRetry: (response, timeSpan) =>
+            //    .WaitAndRetryAsync(maxRetryCount, sleepDurationProvider: retryAttempt =>
             //    {
-            //        Console.WriteLine(retryCount++);
+            //        return TimeSpan.FromSeconds(Math.Pow(2, retryAttempt) / 2);
             //    });
 
-            //Exponential wait and retry policy
-            _httpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
-                .WaitAndRetryAsync(3, sleepDurationProvider: retryAttempt =>
+            ////Exponential wait and retry policy with a fallback policy
+            _httpFallbackPolicy = Policy.HandleResult<HttpResponseMessage>(r => r.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                .FallbackAsync(new HttpResponseMessage(HttpStatusCode.OK)
                 {
-                    return TimeSpan.FromSeconds(Math.Pow(2, retryAttempt) / 2);
+                    Content = new ObjectContent(fallBackNumber.GetType(),fallBackNumber, new JsonMediaTypeFormatter())
                 });
         }
 
-            [HttpGet("{id}")]
+        [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
             var httpClient = new HttpClient();
             string requestEndpoint = "https://" + Request.Host.Value + $"/inventory/{id}";
 
             //HttpResponseMessage response = await httpClient.GetAsync(requestEndpoint);
-            HttpResponseMessage response = await _httpRetryPolicy.ExecuteAsync(() => httpClient.GetAsync(requestEndpoint));
+            //HttpResponseMessage response = await _httpRetryPolicy.ExecuteAsync(() => httpClient.GetAsync(requestEndpoint));
+            HttpResponseMessage response = await _httpFallbackPolicy.ExecuteAsync(
+                ()=> _httpRetryPolicy.ExecuteAsync(
+                    () => httpClient.GetAsync(requestEndpoint)));
 
             if (response.IsSuccessStatusCode)
             {
